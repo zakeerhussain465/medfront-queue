6ab23b9428792fcbe88f5562b1d2fe045f5f9598
Sheik Zakeer Hussain <116176437+zakeerhussain465@users.noreply.github.com>
2025-08-08 01:16:16 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { SpeedInsights } from "@vercel/speed-insights/next"
+
 
 createRoot(document.getElementById("root")!).render(<App />);
